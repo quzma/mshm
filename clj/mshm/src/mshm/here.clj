d0b0513fd4de0874c81e28f2e0c12e1c117bc39d
Ivo Kuzmanovic <ivo.kuzmanovic@here.com>
2021-03-05 19:14:21 +0100

    Update here.clj

--- clj/mshm/src/mshm/here.clj
+++ clj/mshm/src/mshm/here.clj
@@ -4,10 +4,10 @@
     [clojure.data.json          :as json])
  )
 
-(def app-id-weather "nIEVDdnGHY7xgXcD5jdZ")
-(def app-code-weather "E6R9euusf00qbWu6Y-FWyg")
-(def app-id-datalens "ve73EOpIZy3FLGqO4FnW")
-(def app-code-datalens "LTs_gYLjWJTZHbCPcwo2iQ")
+(def app-id-weather "xxx")
+(def app-code-weather "yyyy")
+(def app-id-datalens "zzzz")
+(def app-code-datalens "qqqq")
 
 (defn weather-here
   [lat lon]
